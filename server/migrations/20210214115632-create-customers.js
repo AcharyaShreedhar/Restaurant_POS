cf233f7b463a4d01eac080527cd8b4ab1abd9523
Shreedhar Acharya <spacharya2537@gmail.com>
2021-02-23 16:14:09 +0545

    routes and controllers for Customers CRUD Rest API

--- server/migrations/20210214115632-create-customers.js
+++ server/migrations/20210214115632-create-customers.js
@@ -1,57 +1,59 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('customers', {
+    await queryInterface.createTable("customers", {
       cust_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       cust_name: {
         allowNull: false,
-        type: Sequelize.STRING
-      },
-      cust_phone: {
-        allowNull: false,
-        type: Sequelize.ARRAY(Sequelize.STRING)
+        type: Sequelize.STRING,
       },
       cust_address: {
         allowNull: false,
-        type: Sequelize.JSON
+        type: Sequelize.JSON,
+      },
+      cust_phone: {
+        allowNull: false,
+        type: Sequelize.ARRAY(Sequelize.STRING),
       },
       cust_email: {
         allowNull: false,
-        type: Sequelize.STRING
-      },
-      cust_company: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      cust_dob: {
-        allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.STRING,
       },
       cust_pass: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
-      user_id: {
+      cust_dob: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.DATE,
       },
+      cust_company: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      // user_id: {
+      //   allowNull: false,
+      //   type: Sequelize.INTEGER,
+      // },
 
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('customers');
-  }
+    await queryInterface.dropTable("customers");
+  },
 };
