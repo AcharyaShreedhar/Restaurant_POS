eb81f0124776a861632cdf29ab10f4220e606220
Subash Paudel <subashpaudel144@gmail.com>
2021-04-06 16:49:10 +0545

    updated all the comment messages of inventory category routes

--- server/routes/inventoryCategoryRoute.js
+++ server/routes/inventoryCategoryRoute.js
@@ -4,9 +4,9 @@ const inventoryCategoryController = require("../controllers/inventoryCategoryCon
 
 // items Routes
 router.get("/inventoryCategories", inventoryCategoryController.getAllInventoryCategories);
-router.get("/inventoryCategories/:catId", itemCategoryController.getInventoryCategory);
+router.get("/inventoryCategories/:inventCatId", inventoryCategoryController.getInventoryCategory);
 router.post("/inventoryCategories", inventoryCategoryController.addInventoryCategory);
-router.put("/inventoryCategories/:catId", inventoryCategoryController.updateInventoryCategory);
-router.delete("/inventoryCategories/:catId", inventoryCategoryController.deleteInventoryCategory);
+router.put("/inventoryCategories/:inventCatId", inventoryCategoryController.updateInventoryCategory);
+router.delete("/inventoryCategories/:inventCatId", inventoryCategoryController.deleteInventoryCategory);
 
 module.exports = router;
