d49d781831774366fcc74e6b056bf19a2b1af8f9
anilakheli <kanxoakheli22@gmail.com>
2021-04-09 12:59:24 +0545

    adding table reservation routes

--- server/server.js
+++ server/server.js
@@ -34,6 +34,7 @@ const rolesRoutes = require("./routes/rolesRoutes");
 const offeredItemsRoutes = require("./routes/offeredItemRoutes");
 const offersRoutes = require("./routes/offersRoute");
 const unitPriceRoutes = require("./routes/unitPriceRoutes");
+const tableReservationRoutes = require("./routes/tableReservationRoutes");
 
 app.use("/api/v1", usersRoutes);
 app.use("/api/v1", customersRoutes);
@@ -55,6 +56,7 @@ app.use("/api/v1", rolesRoutes);
 app.use("/api/v1", offeredItemsRoutes);
 app.use("/api/v1", offersRoutes);
 app.use("/api/v1", unitPriceRoutes);
+app.use("/api/v1", tableReservationRoutes);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
