7bdcc4bac46ecd833b5a398c9f8409bebe416729
anilakheli <kanxoakheli22@gmail.com>
2021-04-09 14:10:26 +0545

    done

--- server/controllers/unitPriceController.js
+++ server/controllers/unitPriceController.js
@@ -1,6 +1,6 @@
 const db = require("../db");
 
-//get all restaurants table
+//get all unit Prices
 exports.getAllUnitPrices = async (req, res) => {
   const getAllUnitPricesQuery = "SELECT * FROM unit_prices";
   try {
@@ -17,7 +17,7 @@ exports.getAllUnitPrices = async (req, res) => {
   }
 };
 
-// get a single table
+// get a single unit price
 exports.getUnitPrices = async (req, res) => {
   const getUnitPricesQuery = "SELECT * FROM unit_prices WHERE item_id=$1";
   try {
@@ -36,14 +36,13 @@ exports.getUnitPrices = async (req, res) => {
   }
 };
 
-// add a restaurant table
+// add a unit price 
 exports.addUnitPrices = async (req, res) => {
   const addunitPricesQuery =
-    "INSERT INTO unit_prices (item_id, price, unit) values ($1,$2,$3) returning *";
+    "INSERT INTO unit_prices (price, unit) values ($1,$2) returning *";
 
   try {
-    const results = await db.query(addRestaurantTableQuery, [
-      req.body.item_id,
+    const results = await db.query(addunitPricesQuery, [
       req.body.price,
       req.body.unit,
     ]);
@@ -58,15 +57,15 @@ exports.addUnitPrices = async (req, res) => {
   }
 };
 
-// update table
+// update unit price
 exports.updateUnitPrices = async (req, res) => {
   const updateUnitPricesQuery =
-    "UPDATE unit_prices SET  price=$2, unit=$3, WHERE item_id=$5 returning *";
+    "UPDATE unit_prices SET  price=$1, unit=$2, WHERE item_id=$3 returning *";
   try {
     const results = await db.query(updateUnitPricesQuery, [
       req.body.price,
       req.body.unit,
-      req.body.item_id
+      req.params.item_id
 
     ]);
     res.status(200).json({
