99f3750a28c18d4d84d32d6fa09b052d2d4f37a2
Ashish kc <kecyasis21@gmail.com>
2021-04-11 20:01:38 +0545

    camelcase done

--- server/routes/supplierInventoriesRoutes.js
+++ server/routes/supplierInventoriesRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const supplierinventoriesController = require('../controllers/supplierinventoriesController')
+const supplierinventoriesController = require('../controllers/supplierInventoriesController')
 
 
 // supplierinventories Routes
-router.get('/supplierinventories', supplierinventoriesController.getAllSupplierinventories);
-router.get('/supplierinventories/:supinventId', supplierinventoriesController.getSupplierinventories)
-router.post('/supplierinventories', supplierinventoriesController.addSupplierinventories);
-router.put('/supplierinventories/:supinventId', supplierinventoriesController.updateSupplierinventories);
-router.delete('/supplierinventories/:supinventId', supplierinventoriesController.deleteSupplierinventories);
+router.get('/supplierinventories', supplierInventoriesController.getAllSupplierInventories);
+router.get('/supplierinventories/:supinventId', supplierInventoriesController.getSupplierInventories)
+router.post('/supplierinventories', supplierInventoriesController.addSupplierInventories);
+router.put('/supplierinventories/:supinventId', supplierInventoriesController.updateSupplierInventories);
+router.delete('/supplierinventories/:supinventId', supplierInventoriesController.deleteSupplierInventories);
 
 
 
